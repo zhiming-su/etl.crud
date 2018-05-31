83c498181f64c5c83faba469f13981a23f070c3e
suzhi <suzhi@DESKTOP-3PEB7OE>
2018-05-31 11:08:05 +0800

    commit

---
+++ src/main/java/com/xiyu/schedulix/agent/SpringBoot0011Application.java
@@ -0,0 +1,12 @@
+package com.xiyu.schedulix.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot0011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot0011Application.class, args);
+	}
+}
